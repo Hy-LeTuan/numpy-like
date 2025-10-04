a47ee269ad2f9b6a32fb3a5dc984e49a55874c64
Hy-LeTuan <letuanhy1507@gmail.com>
2025-10-04 16:27:49 -0500

    repackage sources into a single array in meson.build

--- test/test.py
+++ test/test.py
@@ -1,10 +1,5 @@
 import numpy_like as np_like
 
-# print(np_like.int32)
-
 a = np_like.ndarray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtypes="float32")
+
 a.display_shape()
-
-# a.display()
-
-# print(a)
